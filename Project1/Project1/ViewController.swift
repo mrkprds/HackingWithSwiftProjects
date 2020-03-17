c514cd2105a99902d8b532f68f5f15169b51e024
Patrick <markpatrickperdon@gmail.com>
2020-03-17 11:22:26 +0800

    changed projet type from swiftUI to storyboard

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Mark Patrick Perdon on 3/17/20.
+//  Copyright © 2020 Mark Patrick Perdon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
