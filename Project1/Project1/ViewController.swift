8cf6e40f2c73ebbdb044873b2939eaf34bcbfe79
Patrick <markpatrickperdon@gmail.com>
2020-03-18 13:24:17 +0800

    override tableview method to create rows as same index number of pictures array and display file name

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -23,10 +23,15 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
-        
-        print(path)
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
 
