465dd98bbcb183266a914eb376cd8f9505739887
Patrick <markpatrickperdon@gmail.com>
2020-03-19 13:15:46 +0800

    prepared UI

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
