26512c83d080dc371f859d14b08d8dd54a0d8dc9
Patrick <markpatrickperdon@gmail.com>
2020-03-21 21:09:12 +0800

    experimented with view controller and wkwebview

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -7,28 +7,44 @@
 //
 
 import UIKit
-import WebKit
-
-class ViewController: UIViewController, WKNavigationDelegate{
-    var webView: WKWebView!
-    
-    
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        
-        view = webView
-        
-    }
+class ViewController: UIViewController{
+    @IBOutlet var instructionLabel: UILabel!
+    @IBOutlet var urlField: UITextField!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        instructionLabel.text = "Please enter any URL"
         
-        let url = URL(string: "https://google.com")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        super.viewDidLoad()
     }
+    
+    @IBAction func submitURLButton(_ sender: Any) {
 
+        guard let urlStr = urlField?.text, !urlStr.isEmpty else {
+            let ac = UIAlertController(title: "Text Field is Blank", message: "Please enter a url", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            present(ac, animated: true)
+            return
+        }
+        
+        if let vc = storyboard?.instantiateViewController(identifier: "Browser") as?
+        BroswerViewController{
+            
+            //add url validation
+            print("vs")
+            vc.urlString = urlStr
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
+    
+    //implement some form of url validation
+    /*
+    func isValidUrl(urlGiven: String) -> Bool {
+        let regex = "((http|https|ftp)://)?((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: urlGiveb)
+    }
+     */
 }
 
 
