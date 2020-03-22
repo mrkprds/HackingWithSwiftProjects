2b11bcb82cf47cbfaca7a82cb5f6bc18e7268075
Patrick <markpatrickperdon@gmail.com>
2020-03-22 14:30:58 +0800

    added navigation item (unfinished)

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -23,10 +23,17 @@ class ViewController: UIViewController, WKNavigationDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
         let url = URL(string: "https://google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "facebook.com", style: .default, handler: openPage))
     }
 
 }
