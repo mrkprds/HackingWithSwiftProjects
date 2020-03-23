73e2dc653dcc0c8b2c740809c64b75de2d909b75
Patrick <markpatrickperdon@gmail.com>
2020-03-23 16:53:15 +0800

    added ui toolbar containing progress button, spacer, and referesh button
    progress button has a observer has progressView and the value of this progress view is taken from WKWebView.estimatedProgress)
    spacer just takes up every bit of space that it can on a ui toolbar
    refress button is UIBarButtonItem that reloads the page

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate{
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     
     override func loadView() {
@@ -21,13 +22,32 @@ class ViewController: UIViewController, WKNavigationDelegate{
         
     }
     
-    override func viewDidLoad() {
+    override func viewDidLoad() {e
         super.viewDidLoad()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
         let url = URL(string: "https://google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButon = UIBarButtonItem(customView: progressView )
+        
+        toolbarItems = [progressButon,  spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver( self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"{
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
     @objc func openTapped(){
