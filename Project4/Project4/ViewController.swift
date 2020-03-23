765d074139e1f848d6dd369386239a96ac974540
Patrick <markpatrickperdon@gmail.com>
2020-03-23 17:27:05 +0800

    Refactored code
    created a string array containing websites
    iterated through websites and then added a UIAlertAction
    added a method called decidePolicyFor, declared that domains in websites array are the ones only allowed to navigate to

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -12,7 +12,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate{
     var webView: WKWebView!
     var progressView: UIProgressView!
-    
+    var websites = ["apple.com", "facebook.com", "twitter.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -22,7 +22,7 @@ class ViewController: UIViewController, WKNavigationDelegate{
         
     }
     
-    override func viewDidLoad() {e
+    override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
@@ -30,7 +30,7 @@ class ViewController: UIViewController, WKNavigationDelegate{
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
-        let url = URL(string: "https://google.com")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
@@ -52,11 +52,28 @@ class ViewController: UIViewController, WKNavigationDelegate{
     
     @objc func openTapped(){
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "facebook.com", style: .default, handler: openPage))
+        
+        for website in websites{
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host{
+            for website in websites{
+                if host.contains(website){
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
     }
     
     func openPage(action: UIAlertAction){
