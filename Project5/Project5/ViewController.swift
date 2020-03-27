ed51128b4130fc9cc31271f0fe0cf4e762ad1e93
Patrick <markpatrickperdon@gmail.com>
2020-03-28 04:15:08 +0800

    Project 5 Continuation
    Add navigation right bar button, when tapped an alert controller with a text field will appear asking for a answer.
    Created 5 methods promptForAnswer, submit, isPossible, isOriginal, isReal.
    
    promptForAnswer method is called when the user taps on the navigation right bar button item. Once answer is submited, submit method will be called
    submit method takes a parameter of string from the text field from the alert controller runs through 4 if else statements to check if the word can be added. If not it will display what's wrong.
    isPossible method takes the answer and iterates each character to the current word being asked to make an anagram of and return a boolean.
    isOriginal method returns a boolean to checks the answer if it is already usedWords array
    isReal method checks where the submited asnwer is a real word using UITextChecker class and returns a boolean valuee

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+        barButtonSystemItem: .add,
+        target: self,
+        action: #selector(promptForAnswer))
+        
         if let startWordsURl = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            
             if let startWords = try? String(contentsOf: startWordsURl){
                 allWords = startWords.components(separatedBy: "\n")
             }
@@ -24,7 +30,6 @@ class ViewController: UITableViewController {
         if allWords.isEmpty{
             allWords = ["silworm"]
         }
-        
         startGame()
     }
     
@@ -43,7 +48,99 @@ class ViewController: UITableViewController {
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
-
-
+    
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(
+            title: "Enter Answer",
+            message: nil,
+            preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(
+            title: "Submit",
+            style: .default){
+                [weak self, weak ac] _ in
+                
+                guard let answer = ac?.textFields?[0].text else { return }
+                self?.submit(answer)
+        }
+            ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String){
+        let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
+        
+        
+        if !lowerAnswer.isEmpty{
+            if isPossible(word: lowerAnswer){
+                if isOriginal(word: lowerAnswer){
+                    if isReal(word: lowerAnswer){
+                        usedWords.insert(lowerAnswer, at: 0)
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+                        
+                        return
+                    }else{
+                        errorTitle = "Word not recognized"
+                        errorMessage = "tf is that shit??"
+                    }
+                }else{
+                    errorTitle = "Word Used"
+                    errorMessage = "Think outside the box"
+                }
+            }else{
+                errorTitle = "Impossible"
+                errorMessage = "You can't spell that from \(title!.lowercased())"
+            }
+        }else{
+            errorTitle = "Enter something"
+            errorMessage = "wot u think dis a joke???"
+        }
+        let ac = UIAlertController(
+                   title: errorTitle,
+                   message: errorMessage,
+                   preferredStyle: .alert)
+               ac.addAction(UIAlertAction(
+                   title: "Close",
+                   style: .default))
+               present(ac, animated: true)
+    }
+ 
+    func isPossible(word: String) -> Bool{
+        guard var tempWord = title?.lowercased() else {
+            return false
+        }
+        for letter in word{
+            if let position = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: position)
+            }else{
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool{
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
+        //use utf16 for counting words in apple frameworks
+        let range  = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange =  checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        if word.utf16.count == 3{
+            return false
+        }else{
+            
+        }
+        return misspelledRange.location == NSNotFound
+    }
 }
 
