bdae9afc33ca5d1c6a2028a4210e01f3d4658e5d
Patrick <markpatrickperdon@gmail.com>
2020-04-05 03:51:46 +0800

    quick code fix

--- Project7/Project7/SceneDelegate.swift
+++ Project7/Project7/SceneDelegate.swift
@@ -19,12 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = ViewController()    // your ViewController
-        window?.makeKeyAndVisible()
-        
         if let tabBarController = window?.rootViewController as? UITabBarController{
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vc = storyboard.instantiateViewController(identifier: "NavController")
