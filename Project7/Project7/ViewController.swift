af1dc0b2de8530ea52f9373490eb2d947af70236
Patrick <markpatrickperdon@gmail.com>
2020-04-05 21:33:55 +0800

    Added navigation item for showing where data is being pulled, filtering using searching by keywords and reloading the data.

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var urlString = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString: String
+        
+        let search = UIBarButtonItem(
+                barButtonSystemItem: .search,
+                target: self,
+                action: #selector(showSearchFilter))
+        
+        let reload = UIBarButtonItem(
+                barButtonSystemItem: .refresh,
+                target: self,
+                action: #selector(loadParsedData))
+        navigationItem.leftBarButtonItems = [search, reload]
+
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(title: "Credit",
+                            style: .done,
+                            target: self,
+                            action: #selector(showCredits))
         
         if navigationController?.tabBarItem.tag == 0{
             urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
@@ -20,29 +38,79 @@ class ViewController: UITableViewController {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=1000&limit=100"
         }
         
-        if let url = URL(string: urlString){
-            if let data = try? Data(contentsOf: url){
-                parse(json: data)
-                return
-            }
+        loadParsedData()
+    }
+    
+    @objc func showCredits(){
+        let ac = UIAlertController(title: "Date being pulled from:",
+                                   message: urlString,
+                                   preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Close", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func showSearchFilter(){
+        let ac = UIAlertController(title: "Search Filter",
+                                   message: "Enter any keyword pertaining to a title or content of body",
+                                   preferredStyle: .alert)
+        ac.addTextField{
+            (textField) in
+            textField.placeholder = "Enter Keyword title"
+        }
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .cancel) {
+            [weak self, weak ac] _ in
+            guard let searchQuery = ac?.textFields?[0].text, !searchQuery.isEmpty else { return }
+            self?.filter(keyword: searchQuery)
+        }
+        
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func filter(keyword: String?){
+        loadParsedData()
+        if let keyword = keyword, !keyword.isEmpty{
+            let allPetitions = petitions
+            petitions = allPetitions.filter({
+                return $0.title.contains(keyword) || $0.body.contains(keyword)
+            })
+            tableView.reloadData()
+            print(petitions)
+        }else{
+            let ac = UIAlertController(title: "Text field is empty",
+                                       message: "Please enter a keyword",
+                                       preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Close", style: .default))
+            ac.present(ac, animated: true)
         }
     }
     
     func showErr() {
-        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed, please check your conenction.", preferredStyle: .alert)
+        let ac = UIAlertController(
+            title: "Loading Error",
+            message: "There was a problem loading the feed, please check your conenction.",
+            preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Close", style: .default))
         present(ac, animated: true)
     }
     
     func parse(json: Data){
         let deconder = JSONDecoder()
-        
         if let jsonPetitions = try? deconder.decode(Petitions.self, from:  json){
             petitions = jsonPetitions.results
             tableView.reloadData()
         }
-        
-        
+    }
+    
+    @objc func loadParsedData(){
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+                return
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
